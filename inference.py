1c7bb74556ae72b3ffa3fb38c3c4e39d5ff98920
Nguyễn Quang Tính <100455964+tinhpx2911@users.noreply.github.com>
2023-07-19 17:29:03 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -19,7 +19,7 @@ def get_response(prompt, tokenizer, model, generation_config, max_new_tokens):
 
 def generate_inference(instruction: str, user_inp: str, model_path:str, lora_weights_path:str):
     
-    top_k=40
+    top_k=64
     top_p=256
     temperature=0.1
     load_8bit=True
